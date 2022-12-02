242ef2577fec212c6ac74c10d2f03346ac641680
Kjetil Aune <aune.kjetil@gmail.com>
2022-12-02 19:43:59 +0100

    Initial commit

---
+++ 1.py
@@ -0,0 +1,30 @@
+
+
+def task1():
+	myList = [line.rstrip() for line in open('1.txt')]
+	calList = []
+	counter = 0
+	for item in myList:
+		if item == '':
+			calList += [counter]
+			counter = 0
+			continue
+		counter += int(item)
+
+	print max(calList)
+
+
+def task2():
+	myList = [line.rstrip() for line in open('1.txt')]
+	calList = []
+	counter = 0
+	for item in myList:
+		if item == '':
+			calList += [counter]
+			counter = 0
+			continue
+		counter += int(item)
+
+	calList.sort()	
+	print calList[-1] + calList[-2] + calList[-3]
+task2()
---
+++ 2.py
@@ -0,0 +1,61 @@
+#Task 1
+
+def playMatch(opponent, me):
+	plays = ['A', 'B', 'C']
+	
+	meShifted = chr(ord(me) - 23)
+
+	if opponent == meShifted:
+		return "D"
+	elif plays[plays.index(meShifted) - 1] == opponent:
+		return "W"
+	else:
+		return "L"
+
+def main1():
+	myList = [tuple(map(str, line.rstrip().split(" "))) for line in open('2.txt')]
+
+	score = {'X': 1, 'Y': 2, 'Z': 3, 'L': 0, 'D': 3, 'W': 6}
+
+	totalScore = 0
+
+	for match in myList:
+		opponent, me = match[0], match[1]
+
+		totalScore += score[me] + score[playMatch(opponent, me)]
+		
+	print totalScore
+
+#main1()
+
+
+
+
+#Task 2
+
+def findPlay(opponent, result):
+	plays = ['A', 'B', 'C']
+	
+	if result == "X":
+		return plays[plays.index(opponent) - 1]
+	elif result == "Y":
+		return opponent
+	else:
+		return plays[plays.index(opponent) -2]
+
+def main2():
+	myList = [tuple(map(str, line.rstrip().split(" "))) for line in open('2.txt')]
+
+	score = {'X': 0, 'Y': 3, 'Z': 6, 'A': 1, 'B': 2, 'C': 3}
+	totalScore = 0
+
+	for match in myList:
+		opponent, result = match[0], match[1]
+
+		totalScore += score[result] + score[findPlay(opponent, result)]
+
+	print totalScore
+
+	
+
+main2()
